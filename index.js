00ba21336d69700df5268058fbf4688e750b44a7
manojkumarg7 <manojkumargowdru7@gmail.com>
2024-03-21 20:38:48 +0530

    second commits

--- index.js
+++ index.js
@@ -2,3 +2,17 @@ const mobileMenu = document.querySelector(".mobile-menu");
 mobileMenu.addEventListener("click", () => {
   document.querySelector(".menu").classList.toggle("show");
 });
+const scrollBtn = document.querySelector(".scroll-top");
+window.addEventListener("scroll", function () {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    scrollBtn.style.display = "block";
+  } else {
+    scrollBtn.style.display = "none";
+  }
+});
+scrollBtn.addEventListener("click", () => {
+  document.documentElement.scrollTop = 0;
+});
